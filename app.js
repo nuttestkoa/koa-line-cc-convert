94bc2a05ef2176e9e9a37c436a03397979bb1b3e
Pakkanat-M <pakkanat@mango.exchange>
2020-06-08 20:23:46 +0700

    test get token

--- app.js
+++ app.js
@@ -17,26 +17,28 @@ router
     .post('/webhook', async (ctx, next) => {
         ctx.body = JSON.stringify(ctx.request.body);
         console.log(ctx.body);
-        // let headers = {
-        //     'Content-Type': 'application/json',
-        //     'Authorization': 'Bearer {82d6w35tT/ZdYKVd8G6OCOEmY5M+b4SYMBSp0NWilZ1OjW9nQQm2yRBiUcAQiLZ2gF3QApm6caL7EHjynnQGQn+P0kb+T3Qknn7nR3iBCLsQOfMxuyoJOdOrL+ogVX8uvBKBVwTunPeuqdojX77lJgdB04t89/1O/w1cDnyilFU=}'
-        // };
-        // let body = JSON.stringify({
-        //     replyToken: reply_token,
-        //     messages: [{
-        //         type: 'text',
-        //         text: 'Hello'
-        //     },
-        //     {
-        //         type: 'text',
-        //         text: 'How are you?'
-        //     }]
-        // });
-        // router.post({
-        //     url: 'https://api.line.me/v2/bot/message/reply',
-        //     headers: headers,
-        //     body: body
-        // });
+        console.log(ctx.body.events[0].replyToken);
+        let reply_Token = ctx.body.events[0].replyToken;
+        let headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer {82d6w35tT/ZdYKVd8G6OCOEmY5M+b4SYMBSp0NWilZ1OjW9nQQm2yRBiUcAQiLZ2gF3QApm6caL7EHjynnQGQn+P0kb+T3Qknn7nR3iBCLsQOfMxuyoJOdOrL+ogVX8uvBKBVwTunPeuqdojX77lJgdB04t89/1O/w1cDnyilFU=}'
+        };
+        let body = JSON.stringify({
+            replyToken: reply_token,
+            messages: [{
+                type: 'text',
+                text: 'Hello'
+            },
+            {
+                type: 'text',
+                text: 'How are you?'
+            }]
+        });
+        router.post({
+            url: 'https://api.line.me/v2/bot/message/reply',
+            headers: headers,
+            body: body
+        });
     });
 
 app.on('error', (err, ctx) => {
