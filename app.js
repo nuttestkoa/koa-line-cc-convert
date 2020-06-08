32c8aba0746922443c8121ca31dc52b79b0d9bc7
Pakkanat-M <pakkanat@mango.exchange>
2020-06-08 20:16:56 +0700

    log body

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ router
     })
     .post('/webhook', async (ctx, next) => {
         ctx.body = JSON.stringify(ctx.request.body);
+        console.log(ctx.body);
         // let headers = {
         //     'Content-Type': 'application/json',
         //     'Authorization': 'Bearer {82d6w35tT/ZdYKVd8G6OCOEmY5M+b4SYMBSp0NWilZ1OjW9nQQm2yRBiUcAQiLZ2gF3QApm6caL7EHjynnQGQn+P0kb+T3Qknn7nR3iBCLsQOfMxuyoJOdOrL+ogVX8uvBKBVwTunPeuqdojX77lJgdB04t89/1O/w1cDnyilFU=}'
