865ac4065b723e5db56d66411f69cbddcf68c921
Pakkanat-M <pakkanat@mango.exchange>
2020-06-09 22:43:56 +0700

    Fix didn't call fixer get api

--- app.js
+++ app.js
@@ -40,33 +40,36 @@ router
         //         console.log('server error', err, ctx);
         //     });
     })
-    .get('/fixer', async (ctx, next) => {
-        console.log('FIXER API START');
-        ctx.body = ctx;
-        // console.log(ctx.request.body);
-        // http://data.fixer.io/api/latest?access_key=6646d19dc18a481c1fc14f93094a3197
-        var fx_option = {
-                method: 'GET',
-                url: 'http://data.fixer.io/api/latest?access_key=6646d19dc18a481c1fc14f93094a3197',
-                json: true
-            }
-            var fx_rp = require('request-promise');
-            fx_rp(fx_option)
-                .then(function (parsedBody){
-                    console.log('fx rq success');
-                    // console.log('parsed = ' , parsedBody);
-                    console.log('USD = ' , parsedBody.rates.USD);
-                    usd_rates = parsedBody.rates.USD;
-                    console.log('THB = ' , parsedBody.rates.THB);
-                    thb_rates = parsedBody.rates.THB;
-                    usb_to_thb = (1 / usd_rates) * thb_rates;
-                })
-                .catch(function (err) {
-                    console.log('server error', err, ctx);
-                });
+    // .get('/fixer', async (ctx, next) => {
+    //     console.log('FIXER API START');
+    //     ctx.body = ctx;
+    //     // console.log(ctx.request.body);
+    //     // http://data.fixer.io/api/latest?access_key=6646d19dc18a481c1fc14f93094a3197
         
-    })
+        
+    // })
     .post('/webhook', async (ctx, next) => {
+        var fx_option = {
+            method: 'GET',
+            url: 'http://data.fixer.io/api/latest?access_key=6646d19dc18a481c1fc14f93094a3197',
+            json: true
+        }
+        var fx_rp = require('request-promise');
+        fx_rp(fx_option)
+            .then(function (parsedBody){
+                console.log('fx rq success');
+                // console.log('parsed = ' , parsedBody);
+                console.log('USD = ' , parsedBody.rates.USD);
+                usd_rates = parsedBody.rates.USD;
+                console.log('THB = ' , parsedBody.rates.THB);
+                thb_rates = parsedBody.rates.THB;
+                usb_to_thb = (1 / usd_rates) * thb_rates;
+            })
+            .catch(function (err) {
+                console.log('server error', err, ctx);
+            });
+
+
         var reply_Token = ctx.request.body.events[0].replyToken;
         var receive_Text = ctx.request.body.events[0].message.text;
         if(reply_Token === '00000000000000000000000000000000') {
@@ -75,10 +78,6 @@ router
         var rp_body = {
             replyToken: reply_Token,
             messages: [{
-                    type: 'text',
-                    text: 'Hello'
-                },
-                {
                     type: 'text',
                     text: 'Hello'
                 },
@@ -119,60 +118,3 @@ router
 
 app.listen(port);
 module.exports = { app }
-
-// console.log('Listening to %s', port);
-
-// const express = require('express')
-// const bodyParser = require('body-parser')
-// const request = require('request')
-// const app = express()
-// const port = process.env.PORT || 4000
-// app.use(bodyParser.urlencoded({ extended: false }))
-// app.use(bodyParser.json())
-// app.post('/webhook', (req, res) => {
-//     let reply_token = req.body.events[0].replyToken
-//     reply(reply_token)
-//     res.sendStatus(200)
-// })
-// app.listen(port)
-// function reply(reply_token) {
-//     let headers = {
-//         'Content-Type': 'application/json',
-//         'Authorization': 'Bearer {82d6w35tT/ZdYKVd8G6OCOEmY5M+b4SYMBSp0NWilZ1OjW9nQQm2yRBiUcAQiLZ2gF3QApm6caL7EHjynnQGQn+P0kb+T3Qknn7nR3iBCLsQOfMxuyoJOdOrL+ogVX8uvBKBVwTunPeuqdojX77lJgdB04t89/1O/w1cDnyilFU=}'
-//     }
-//     let body = JSON.stringify({
-//         replyToken: reply_token,
-//         messages: [{
-//             type: 'text',
-//             text: 'Hello'
-//         },
-//         {
-//             type: 'text',
-//             text: 'How are you?'
-//         }]
-//     })
-//     request.post({
-//         url: 'https://api.line.me/v2/bot/message/reply',
-//         headers: headers,
-//         body: body
-//     }, (err, res, body) => {
-//         console.log('status = ' + res.statusCode);
-//     });
-// }
-
-// router.get('/', (ctx, next) => {
-//     console.log(ctx);
-//     ctx.body = ctx;
-// });
-
-// app.use(async (ctx, next) => {
-//     try {
-//         await next();
-//     } catch (err) {
-//         ctx.status = err.status || 500;
-//         ctx.body = err.message;
-//         ctx.app.emit('error', err, ctx);
-//     }
-// });
-// const server = app.listen(3000);
-// module.exports = server;
