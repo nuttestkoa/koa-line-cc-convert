80a140375893c7c49d5c4a7b321e4d90a97bf543
Pakkanat-M <pakkanat@mango.exchange>
2020-06-10 20:30:07 +0700

    implement convert to USD and JPY

--- routes/webhooks.js
+++ routes/webhooks.js
@@ -4,7 +4,8 @@ module.exports = ({ router }) => {
     var thb_rates = 0.0;
     var jpy_rates = 0.0;
 
-    var usb_to_thb = 0.0;
+    var usd_to_thb = 0.0;
+    var jpy_to_thb = 0.0;
 
     router.post('/webhook', async (ctx, next) => {
         let fx_option = {
@@ -23,36 +24,81 @@ module.exports = ({ router }) => {
                 thb_rates = parsedBody.rates.THB;
 
                 jpy_rates = parsedBody.rates.JPY;
-                usb_to_thb = (1 / usd_rates) * thb_rates;
+                usd_to_thb = (1 / usd_rates) * thb_rates;
+                jpy_to_thb = (1 / jpy_rates) * thb_rates;
             })
             .catch(function (err) {
                 console.log('server error', err, ctx);
             });
 
-
         let reply_Token = ctx.request.body.events[0].replyToken;
-        let receive_Text = ctx.request.body.events[0].message.text;
+       
         if(reply_Token === '00000000000000000000000000000000') {
             ctx.status = 200;
         } else {
-            let rp_body = {
-            replyToken: reply_Token,
-            messages: [{
-                    type: 'text',
-                    text: 'Hello'
-                },
-                {
-                    type: 'text',
-                    text: 'USD rates : ' + usb_to_thb.toFixed(2)
-                },
-                {
-                    type: 'text',
-                    text: 'I will echo your text :'
-                },
-                {
-                    type: 'text',
-                    text: receive_Text
-                }]
+
+            let reply_Type = ctx.request.body.events[0].type;
+
+            if( reply_Type === 'text') {
+                
+                let receive_Text = ctx.request.body.events[0].message.text;
+                let lower_receive_Text = receive_Text.toLowerCase();
+                let echo_Text = ["echo", "Echo", "ECHO"];
+                let reply_Text = [];
+                if( lower_receive_Text.includes('echo')) {
+                    let rp_body = {
+                        reply_Text = [{
+                                type: 'text',
+                                text: 'Hello, I will echo your text :'
+                            },
+                            {
+                                type: 'text',
+                                text: receive_Text
+                            }]
+                        }
+                } else if(
+                    hasNumbers(receive_Text) &&
+                    lower_receive_Text.includes('usd') ||
+                    lower_receive_Text.includes('jpy') 
+                    ) {
+                        convert_number = receive_Text.match(/\d+/)[0];
+                        switch (lower_receive_Text) {
+                            case 'usd':
+                                let converted_usd = convert_number*usd_to_thb
+                                reply_Text = [{
+                                    type: 'text',
+                                    text: convert_number + ' USD is ' + converted_usd + ' THB'
+                                }]
+                                break;
+                            case 'jpy':
+                                let converted_jpy = convert_number*jpy_to_thb
+                                reply_Text = [{
+                                    type: 'text',
+                                    text: convert_number + ' JPY is ' + converted_jpy + ' THB'
+                                }]
+                                break;
+                            default:
+                                reply_Text = [{
+                                    type: 'text',
+                                    text: 'Default THB'
+                                }]
+                                break;
+                        }
+                } else {
+                    reply_Text = [{
+                        type: 'text',
+                        text: 'Welcome to currency converter by Nut'
+                    }]
+                
+                }
+
+            } else {
+
+                let rp_body = {
+                    replyToken: reply_Token,
+                    messages: reply_Text
+                }
+
             }
 
             let options = {
@@ -76,3 +122,9 @@ module.exports = ({ router }) => {
         }
     })
 };
+
+function hasNumbers(t)
+{
+    var regex = /\d/g;
+    return regex.test(t);
+}   
